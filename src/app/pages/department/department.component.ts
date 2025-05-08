96ed0d78a0cda56e0e3f177bb7d573a0d75bcf2a
mattia <m.colucci@studenti.apuliadigitalmaker.it>
2025-05-08 12:24:22 +0200

    non funziona la free api

--- src/app/pages/department/department.component.ts
+++ src/app/pages/department/department.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MasterService } from '../../services/master.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-department',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './department.component.html',
   styleUrl: './department.component.scss'
 })
-export class DepartmentComponent {
+export class DepartmentComponent implements OnInit{
+
+  masterSrc = inject(MasterService)
+  deptList: any[] = []
+  ngOnInit(): void {
+    this.getDept();
+  }
+
+  getDept() {
+    this.masterSrc.getAllDepartment().subscribe((res:any)=>{
+      debugger;
+      this.deptList = res.data;
+    })
+  }
 
 }
